35e35bf5ae2e2e2f93860ac11352a077942c30e3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-11 22:14:44 -0300

    Exercicio finalizado - 11/10/2024

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+double a, b, c, calculo, x1, x2;
+
+Console.WriteLine("-- Equação de Segundo Grau --");
+Console.WriteLine();
+Console.Write("(A):... ");
+a = Convert.ToDouble (Console.ReadLine());
+Console.Write("(B):... ");
+b = Convert.ToDouble (Console.ReadLine());
+Console.Write("(C):... ");
+c = Convert.ToDouble (Console.ReadLine());
+
+
+if (a == 0)
+
+{
+    Console.WriteLine("Não é equação de segundo grau");
+}
+else 
+{
+    calculo = b * b - 4 * a * c;
+
+    if (calculo < 0)
+    {
+        Console.WriteLine($"Como delta = {calculo:N2}, a equação não possui raizes reais! ");
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(calculo)) / (2 * a);
+        x2 = (-b - Math.Sqrt(calculo)) / (2 * a);
+
+        Console.WriteLine($"x1 = {x1:N2} e x2 + {x2:N2}");
+    }
+}
